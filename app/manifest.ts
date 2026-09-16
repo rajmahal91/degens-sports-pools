56af3f7ad597bd9ac79ad7c5d044a4b33b2a9f23
rajmahal91 <raj_mahal91@hotmail.com>
2026-09-16 00:33:32 -0700

    Point app manifest to Sports Syndicate Fantasy logo

--- app/manifest.ts
+++ app/manifest.ts
@@ -6,9 +6,9 @@ export default function manifest():MetadataRoute.Manifest{
     start_url:'/',scope:'/',display:'standalone',background_color:'#080a0f',theme_color:'#080a0f',orientation:'portrait-primary',
     categories:['sports','entertainment'],
     icons:[
-      {src:'/icons/degens-192.png',sizes:'192x192',type:'image/png',purpose:'any'},
-      {src:'/icons/degens-512.png',sizes:'512x512',type:'image/png',purpose:'any'},
-      {src:'/icons/degens-maskable-512.png',sizes:'512x512',type:'image/png',purpose:'maskable'},
+      {src:'/icons/sports-syndicate-192.png',sizes:'192x192',type:'image/png',purpose:'any'},
+      {src:'/icons/sports-syndicate-512.png',sizes:'512x512',type:'image/png',purpose:'any'},
+      {src:'/icons/sports-syndicate-maskable-512.png',sizes:'512x512',type:'image/png',purpose:'maskable'},
     ],
   };
 }
